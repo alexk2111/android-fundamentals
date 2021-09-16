d9343d500fb174b56c301d90e174bef94c3eee06
Aleksandr Kucherenko <alexk2111@ukr.net>
2021-09-16 17:15:15 +0300

    Initial commit

---
+++ StartAndroid/app/src/main/java/com/sigmasoftwere/akucherenko/startandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sigmasoftwere.akucherenko.startandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
