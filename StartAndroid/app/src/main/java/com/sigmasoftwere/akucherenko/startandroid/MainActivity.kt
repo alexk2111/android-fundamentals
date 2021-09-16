450ca19bc9838ca295d40dffa37c37b5e9e2699a
Aleksandr Kucherenko <alexk2111@ukr.net>
2021-09-16 23:03:56 +0300

    Add Buttons in constraintlayout, add function showToast and onClick to buttons

--- StartAndroid/app/src/main/java/com/sigmasoftwere/akucherenko/startandroid/MainActivity.kt
+++ StartAndroid/app/src/main/java/com/sigmasoftwere/akucherenko/startandroid/MainActivity.kt
@@ -2,10 +2,32 @@ package com.sigmasoftwere.akucherenko.startandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity::class.java.simpleName
+    private var mCount = 0
+    private lateinit var mShowCount: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(LOG_TAG, "Hello World");
+
+        mShowCount = findViewById(R.id.tv_show_count)
+        mCount = getString(R.string.count_initial_value).toInt()
     }
+
+    fun showToast(view: android.view.View) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+
+    }   
+    fun countUp(view: android.view.View) {
+        mCount++
+        mShowCount.text = mCount.toString()
+    }
+
 }
