816556f6f022ff91826c79ed0e4c9bbad1dbdeac
Aleksandr Kucherenko <alexk2111@ukr.net>
2021-09-17 18:20:56 +0300

    modify second activity for return result

--- TwoActivities/app/src/main/java/com/sigmasoftwere/akucherenko/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/com/sigmasoftwere/akucherenko/twoactivities/MainActivity.kt
@@ -4,21 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.content.Intent
+import android.widget.EditText
 
 
-
-
+const val EXTRA_MESSAGE = "com.sigmasoftwere.akucherenko.twoactivities.extra.MESSAGE"
 class MainActivity : AppCompatActivity() {
     private val LOG_TAG = MainActivity::class.java.simpleName
+    private lateinit var mMessageEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        mMessageEditText = findViewById(R.id.editText_main)
     }
 
     fun launchSecondActivity(view: android.view.View) {
         Log.d(LOG_TAG, "Button clicked!")
         val intent = Intent(this, SecondActivity::class.java)
+        val message = mMessageEditText.getText().toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
         startActivity(intent)
     }
 }
--- TwoActivities/app/src/main/java/com/sigmasoftwere/akucherenko/twoactivities/SecondActivity.kt
+++ TwoActivities/app/src/main/java/com/sigmasoftwere/akucherenko/twoactivities/SecondActivity.kt
@@ -1,11 +1,33 @@
 package com.sigmasoftwere.akucherenko.twoactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+const val  EXTRA_REPLY = "com.sigmasoftwere.akucherenko.twoactivities.extra.REPLY"
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var messageText: TextView
+    private lateinit var mReply: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        messageText = findViewById(R.id.text_message)
+        messageText.text = message
+
+        mReply = findViewById(R.id.editText_second)
+    }
+
+    fun launchMainActivity(view: android.view.View) {
+        val reply = mReply.getText().toString()
+        val replyIntent = Intent()
+        replyIntent.putExtra(EXTRA_REPLY, reply)
+        setResult(RESULT_OK,replyIntent)
+        finish()
     }
 }
