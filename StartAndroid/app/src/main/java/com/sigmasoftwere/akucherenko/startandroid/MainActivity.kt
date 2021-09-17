a610f5297857998fcba5c8cc6e62589d76c012cf
Aleksandr Kucherenko <alexk2111@ukr.net>
2021-09-17 11:22:20 +0300

    Add reset button

--- StartAndroid/app/src/main/java/com/sigmasoftwere/akucherenko/startandroid/MainActivity.kt
+++ StartAndroid/app/src/main/java/com/sigmasoftwere/akucherenko/startandroid/MainActivity.kt
@@ -3,8 +3,10 @@ package com.sigmasoftwere.akucherenko.startandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 private const val KEY_COUNT = "count"
 
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private val LOG_TAG = MainActivity::class.java.simpleName
     private var mCount = 0
     private lateinit var mShowCount: TextView
+    private lateinit var mReset: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,26 +23,55 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(LOG_TAG, "Hello World")
 
-        mCount = savedInstanceState?.getInt(KEY_COUNT, 0) ?:
-        getString(R.string.count_initial_value).toInt()
+        mCount = savedInstanceState?.getInt(KEY_COUNT, 0)
+            ?: getString(R.string.count_initial_value).toInt()
 
         mShowCount = findViewById(R.id.tv_show_count)
         mShowCount.text = mCount.toString()
+
+        mReset = findViewById(R.id.button_reset)
+        setResetColor()
+
 //        mCount = getString(R.string.count_initial_value).toInt()
     }
 
     fun showToast(view: android.view.View) {
         Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
 
-    }   
+    }
+
     fun countUp(view: android.view.View) {
         mCount++
         mShowCount.text = mCount.toString()
+        setResetColor()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
         outState.putInt(KEY_COUNT, mCount)
+    }
+
+    fun resetCount(view: android.view.View) {
+        mCount = 0
+        mShowCount.text = mCount.toString()
+        setResetColor()
+    }
+
+    fun setResetColor() {
+        if (mCount > 0)
+            mReset.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.design_default_color_primary
+                )
+            )
+        else
+            mReset.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.material_on_background_disabled
+                )
+            )
     }
 }
