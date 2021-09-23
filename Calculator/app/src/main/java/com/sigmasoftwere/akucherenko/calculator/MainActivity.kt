cdeef669bd4b7e7be44fd9ae8f70c3fa9ecc7bfd
Aleksandr Kucherenko <alexk2111@ukr.net>
2021-09-23 23:19:30 +0300

    Add function for click number and dot buttons

--- Calculator/app/src/main/java/com/sigmasoftwere/akucherenko/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/sigmasoftwere/akucherenko/calculator/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.sigmasoftwere.akucherenko.calculator.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private val resultCalculate: Double = 0.0
     private lateinit var resultTextView: TextView
     private lateinit var operationTextView: TextView
+    private var lastOperation: String = ""
+    private var operation = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +27,24 @@ class MainActivity : AppCompatActivity() {
         resultTextView = binding.resultCalculate
         operationTextView = binding.operationCalculate
 
-        resultTextView.text = "0"
-        operationTextView.text = "0"
+        resultTextView.text = ""
+        operationTextView.text = ""
+        lastOperation = "="
+        operation = 0
     }
 
+    fun onNumberClick(view: android.view.View) {
+        val button: Button = view as Button
+        resultTextView.text = resultTextView.text.toString() + button.text.toString()
+    }
+
+    fun onDotClick(view: android.view.View) {
+
+//        Toast.makeText(this,
+//            resultTextView.text.toString().lastIndexOf(".").toString(),
+//            Toast.LENGTH_SHORT ).show()
+        if (resultTextView.text.toString().lastIndexOf(".") == -1) {
+            resultTextView.text = resultTextView.text.toString() + "."
+        }
+    }
 }
