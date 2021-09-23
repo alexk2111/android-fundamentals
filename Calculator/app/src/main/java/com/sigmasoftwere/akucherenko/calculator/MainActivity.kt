90ffd8f2133cc3730cb68944b3fc4468645d6d3f
Aleksandr Kucherenko <alexk2111@ukr.net>
2021-09-23 21:27:04 +0300

    Build simple ui

--- Calculator/app/src/main/java/com/sigmasoftwere/akucherenko/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/sigmasoftwere/akucherenko/calculator/MainActivity.kt
@@ -2,10 +2,30 @@ package com.sigmasoftwere.akucherenko.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.sigmasoftwere.akucherenko.calculator.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val resultCalculate: Double = 0.0
+    private lateinit var resultTextView: TextView
+    private lateinit var operationTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        resultTextView = binding.resultCalculate
+        operationTextView = binding.operationCalculate
+
+        resultTextView.text = "0"
+        operationTextView.text = "0"
     }
+
 }
